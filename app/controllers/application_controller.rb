ad9a906c58648c75d768037fd18ae608d523e943
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2016-01-15 15:12:35 +0000

    committing change to log in functionality allowing email or username to be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
 
 
   def configure_permitted_parameters
-    extra_fields = [:name, :username, :role_id, :user_image, :user_location]
+    extra_fields = [:username, :first_name, :last_name, :steam_id, :origin_id, :uplay_id, :xbox_user, :psn, :nnid]
     extra_fields.each { |field| devise_parameter_sanitizer.for(:sign_up) << field }
     extra_fields.each { |field| devise_parameter_sanitizer.for(:account_update) << field }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+        devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, 
+  :validatable, :authentication_keys => [:login]
 
   has_many :comments
   has_many :reviews
@@ -35,7 +36,35 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   validates :username, uniqueness: true
+  validates :username, :case_sensitive => false
+  validate :validate_username
+
   validates :email, uniqueness: true
   validates :password, length: { minimum: 8 }
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_h).first
+  end
 
 end
