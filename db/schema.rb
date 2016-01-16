7a575cdb7087318b794edf4e0cf0386fcbd95b4b
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2016-01-16 12:22:26 +0000

    committing seed data for platforms/games and adding checkboxes to edit user page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115131353) do
+ActiveRecord::Schema.define(version: 20160116120418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20160115131353) do
     t.datetime "release_date"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "name"
   end
 
   create_table "group_users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Platform.delete_all
+Platform.create!(name: "PlayStation", owner: "Sony", released: "1995")
+Platform.create!(name: "PlayStation 2", owner: "Sony", released: "2000")
+Platform.create!(name: "PlayStation Portable", owner: "Sony", released: "2005")
+Platform.create!(name: "PlayStation 3", owner: "Sony", released: "2007")
+Platform.create!(name: "PlayStation Vita", owner: "Sony", released: "2012")
+Platform.create!(name: "PlayStation 4", owner: "Sony", released: "2013")
+Platform.create!(name: "Nintendo 64", owner: "Nintendo", released: "1997")
+Platform.create!(name: "GameCube", owner: "Nintendo", released: "2002")
+Platform.create!(name: "Nintendo DS", owner: "Nintendo", released: "2005")
+Platform.create!(name: "Wii", owner: "Nintendo", released: "2006")
+Platform.create!(name: "Nintendo 3DS", owner: "Nintendo", released: "2011")
+Platform.create!(name: "Wii U", owner: "Nintendo", released: "2012")
+Platform.create!(name: "DreamCast", owner: "Sega", released: "1999")
+Platform.create!(name: "Xbox", owner: "Microsoft", released: "2002")
+Platform.create!(name: "Xbox 360", owner: "Microsoft", released: "2005")
+Platform.create!(name: "Xbox One", owner: "Microsoft", released: "2013")
+Platform.create!(name: "PC", owner: "Master Race", released: "0000")
+Platform.create!(name: "Mac", owner: "Apple", released: "0000")
+
+Game.delete_all
+Game.create!(name: "Halo 3", genre: "First-Person Shooter", developer: "Bungie", publisher: "Microsoft", release_date: "26 Sep 2007")
+Game.create!(name: "Call of Duty 4: Modern Warfare", genre: "First-Person Shooter", developer: "Infinity Ward", publisher: "Activision", release_date: "9 Nov 2007")
+Game.create!(name: "Gears of War 3", genre: "Third-Person Shooter", developer: "Epic Games", publisher: "Microsoft", release_date: "20 Sep 2011")
+Game.create!(name: "Grand Theft Auto V", genre: "Action-Adventure", developer: "Rockstar North", publisher: "Rockstar Games", release_date: "17 Sep 2013")
+Game.create!(name: "Assassin's Creed: Brotherhood", genre: "Action-Adventure, Stealth", developer: "Ubisoft Montreal", publisher: "Ubisoft", release_date: "19 Nov 2010")
