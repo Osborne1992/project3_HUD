ea8ec99f2b48ad76d2dd20c137fe51729488e270
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2016-01-21 14:57:11 +0000

    adding description field to groups and links to new comments on event/group/review/tournament/user pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,42 +1,48 @@
-def index
-  @comments = Comment.all.order(:created_at)
-end
+class CommentsController < ApplicationController
 
-def show
-  @comment = find_comment
-end
+  load_and_authorize_resource
 
-def new
-  @comment = current_user.comments.new
-end
+    def index
+      @comments = Comment.all.order(:created_at)
+    end
 
-def create
-  comment = current_user.created_comments.create(comment_params)
-  redirect_to( comment_path( comment.id ) )
-end
+    def show
+      @comment = find_comment
+    end
 
-def destroy
-  comment = find_comment
-  comment.destroy
-  redirect_to(comments_path)
-end
+    def new
+      @comment = current_user.comments.new
+    end
 
-def edit
-  @comment = find_comment
-end
+    def create
+      comment = current_user.created_comments.create(comment_params)
+      redirect_to( comment_path( comment.id ) )
+    end
 
-def update
-  comment = find_comment
-  comment.update(comment_params)
-  redirect_to(comment_path(comment.id))
-end
+    def destroy
+      comment = find_comment
+      comment.destroy
+      redirect_to(comments_path)
+    end
 
-private
+    def edit
+      @comment = find_comment
+    end
 
-def find_comment
-  Comment.find(params[:id])
-end
+    def update
+      comment = find_comment
+      comment.update(comment_params)
+      redirect_to(comment_path(comment.id))
+    end
 
-def comment_params
-  params.require(:comment).permit(:creator_id, :event_id, :group_id, :review_id, :tournament_id, :user_id, :body, :plus_one, :minus_one)
+    private
+
+    def find_comment
+      Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:creator_id, :event_id, :group_id, :review_id, :tournament_id, :user_id, :body, :plus_one, :minus_one)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121143735) do
+ActiveRecord::Schema.define(version: 20160121145640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,8 +126,9 @@ ActiveRecord::Schema.define(version: 20160121143735) do
     t.string   "name"
     t.integer  "creator_id"
     t.boolean  "private"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "platform_users", force: :cascade do |t|
